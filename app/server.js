3b351fe421a733ac143054f934c2cba55196944c
Zifran Chowdhury <zifchowdhury13@gmail.com>
2026-04-07 17:58:01 -0600

    Fix proxy trust

--- app/server.js
+++ app/server.js
@@ -10,6 +10,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
 app.use(helmet({ contentSecurityPolicy: false }));
 
 // Connect to MongoDB
